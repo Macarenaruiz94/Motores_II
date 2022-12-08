a452d533c7aaebabbc3c86955dcc0fb16fbb66f4
Macarena Ruiz <contactomruiz@yahoo.com>
2022-12-08 15:55:11 -0300

    menu

---
+++ Final_Motores_II/Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private string scene;
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
